19afbe5bbfbfd31a6bc8e2fd5e3939be4c5f7e1a
Rupen-project <rupenrakholiya559@gmail.com>
2023-04-21 18:33:21 +0530

    AFI-0014 logger added in admin controller

--- src/main/java/com/spe/jairu/controller/AdminController.java
+++ src/main/java/com/spe/jairu/controller/AdminController.java
@@ -2,6 +2,8 @@ package com.spe.jairu.controller;
 
 import com.spe.jairu.customModel.UserModel;
 import com.spe.jairu.service.admin.AdminService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,6 +17,8 @@ import java.util.Map;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private static final Logger logger = LogManager.getLogger("ProjectManagementSystem");
+
     @Autowired
     AdminService adminService;
 
@@ -24,8 +28,10 @@ public class AdminController {
     {
         try {
             List<UserModel> managers = adminService.getAllManager();
+            logger.info("[AdminController] - [Get All Manager]");
             return ResponseEntity.ok(managers);
         }catch(Exception e){
+            logger.error("[AdminController] - [Error in Get All Manager]");
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
@@ -36,8 +42,10 @@ public class AdminController {
     {
         try {
             List<UserModel> emps = adminService.getAllEmployee();
+            logger.info("[AdminController] - [Get All Employee]");
             return ResponseEntity.ok(emps);
         }catch (Exception e){
+            logger.error("[AdminController] - [Error in Get All Employee]");
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
@@ -48,8 +56,10 @@ public class AdminController {
     {
         try {
             Map<String,String> res = adminService.deleteUser(param);
+            logger.info("[AdminController] - [Delete User]");
             return ResponseEntity.ok(res);
         }catch (Exception e){
+            logger.error("[AdminController] - [Error in Delete User]");
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
